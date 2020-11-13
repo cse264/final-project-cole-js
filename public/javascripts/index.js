ec114e15c29710fbd683383e55e26b3f3f053614
Cole Buck <67663402+ColeBuck@users.noreply.github.com>
2020-11-13 17:42:54 -0500

    basic homepage working

---
+++ public/javascripts/index.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+
+    document.getElementById("signin")
+    .addEventListener("click",function(e){
+
+    });
+    
+    document.getElementById("signout")
+    .addEventListener("click",function(e){
+
+    });
+}
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Cash Clicker' });
+  res.render('index', { title: 'Cash Clicker', user_id: '' });
+});
+
+/* GET home page (signed in). */
+router.get('/signedin', function(req, res, next) {
+  res.render('index', { title: 'Cash Clicker', user_id: '1' });
 });
 
 module.exports = router;
