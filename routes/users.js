c11de4059fbe353c909a5c11ceae51dbd4bade66
Cole Buck <67663402+ColeBuck@users.noreply.github.com>
2020-11-15 03:25:56 -0500

    add basic user profiles

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
+const { Pool } = require('pg')
+const pool = new Pool({
+  host: 'ec2-34-232-24-202.compute-1.amazonaws.com',
+  user: 'enrudtpvrsgwum',
+  database: 'd1d9qff1vs7qpm',
+  password: 'b7acfced35709d3c415d928254be7a310b325a45de5e117e242ff66185ddd7db',
+  port: 5432,
+  ssl: { rejectUnauthorized: false }
+});
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.get('/:username', function(req, res, next) {
+  pool.query("SELECT * FROM users WHERE username=$1", [req.params.username], function(err, result) {
+    if(err) {
+      res.status(404).send();
+    } else {
+      res.render('user', {username: result.rows[0].username, date: result.rows[0].date_created, cash: result.rows[0].cash });
+    }
+  });
 });
 
 module.exports = router;
